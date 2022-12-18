38a7e0aadd2d183b83bfbec6cbf4d745a6bba3e7
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2022-12-18 02:18:43 +0200

    add login form

--- App.js
+++ App.js
@@ -1,10 +1,79 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Keyboard,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from "react-native";
+import image from "./assets/Images/zvezdy.jpg";
 
 export default function App() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const onCloseKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const onSubmit = () => {
+    console.log(email);
+    console.log(password);
+
+    Keyboard.dismiss();
+    setEmail(null);
+    setPassword(null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableWithoutFeedback onPress={onCloseKeyboard}>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+          >
+            <View style={styles.headerTitle}>
+              <Text style={styles.headerTitleText}>LOGIN</Text>
+            </View>
+            <SafeAreaView style={styles.form}>
+              <View>
+                <Text
+                  style={styles.inputTitle}
+                  autoComplete="email"
+                  keyboardType="numeric"
+                >
+                  Email
+                </Text>
+                <TextInput
+                  style={styles.input}
+                  value={email}
+                  onChangeText={setEmail}
+                />
+              </View>
+              <View style={{ marginTop: 16 }}>
+                <Text style={styles.inputTitle}>Password</Text>
+                <TextInput
+                  style={styles.input}
+                  secureTextEntry
+                  value={password}
+                  onChangeText={setPassword}
+                />
+              </View>
+              <View style={styles.button}>
+                <Button title="Log in" onPress={onSubmit} />
+              </View>
+            </SafeAreaView>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +82,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+
+  headerTitle: {
+    marginBottom: 50,
+    alignItems: 'center'
+  },
+  headerTitleText: {
+    fontSize: 60,
+    color: "#f0f8ff",
+  },
+  form: {
+    alignItems: "center",
+  },
+  input: {
+    width: 300,
+    backgroundColor: "#f0f8ff",
+    paddingHorizontal: 16,
+  },
+  inputTitle: {
+    marginBottom: 8,
+    color: "#f0f8ff",
+  },
+  button: {
+    marginTop: 16,
+    width: 200,
   },
 });
