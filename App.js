a15211bebcc8aa3767baea272af9c689fc7eae8b
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-22 01:31:05 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,116 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const AuthStack = createNativeStackNavigator();
+
 import {
   StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  SafeAreaView,
-  TextInput,
-  Button,
-  Keyboard,
-  Platform,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Dimensions,
 } from "react-native";
-import image from "./assets/Images/zvezdy.jpg";
+import LoginScreen from "./screens/auth/LoginScreen";
+import RegisterScreen from "./screens/auth/RegisterScreen";
 
 export default function App() {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
 
-  const onCloseKeyboard = () => {
-    Keyboard.dismiss();
-  };
-
-  const onSubmit = () => {
-    console.log(email);
-    console.log(password);
-
-    Keyboard.dismiss();
-    setEmail(null);
-    setPassword(null);
-  };
 
   return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={onCloseKeyboard}>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-          >
-            <View style={styles.headerTitle}>
-              <Text style={styles.headerTitleText}>LOGIN</Text>
-            </View>
-            <SafeAreaView style={styles.form}>
-              <View>
-                <Text
-                  style={styles.inputTitle}
-                  autoComplete="email"
-                  keyboardType="numeric"
-                >
-                  Email
-                </Text>
-                <TextInput
-                  style={styles.input}
-                  value={email}
-                  onChangeText={setEmail}
-                />
-              </View>
-              <View style={{ marginTop: 16 }}>
-                <Text style={styles.inputTitle}>Password</Text>
-                <TextInput
-                  style={styles.input}
-                  secureTextEntry
-                  value={password}
-                  onChangeText={setPassword}
-                />
-              </View>
-              <View style={styles.button}>
-                <Button title="Log in" onPress={onSubmit} />
-              </View>
-            </SafeAreaView>
-          </KeyboardAvoidingView>
-        </ImageBackground>
-      </TouchableWithoutFeedback>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <AuthStack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+      </AuthStack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  headerTitle: {
-    marginBottom: 50,
-    alignItems: 'center'
-  },
-  headerTitleText: {
-    fontSize: 60,
-    color: "#f0f8ff",
-  },
-  form: {
-    alignItems: "center",
-  },
-  input: {
-    width: 300,
-    backgroundColor: "#f0f8ff",
-    paddingHorizontal: 16,
-  },
-  inputTitle: {
-    marginBottom: 8,
-    color: "#f0f8ff",
-  },
-  button: {
-    marginTop: 16,
-    width: 200,
-  },
+ 
 });
