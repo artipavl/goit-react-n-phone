3de0b8d26b1952afa56980e9daa8f93c8ca2ff10
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-22 03:59:41 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-
 const AuthStack = createNativeStackNavigator();
 
-import {
-  StyleSheet,
-} from "react-native";
+import { StyleSheet } from "react-native";
 import LoginScreen from "./screens/auth/LoginScreen";
 import RegisterScreen from "./screens/auth/RegisterScreen";
 
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <AuthStack.Navigator>
         <AuthStack.Screen
-          name="Login"
-          component={LoginScreen}
+          name="Register"
+          component={RegisterScreen}
           options={{ headerShown: false }}
         />
         <AuthStack.Screen
-          name="Register"
-          component={RegisterScreen}
+          name="Login"
+          component={LoginScreen}
           options={{ headerShown: false }}
         />
       </AuthStack.Navigator>
@@ -31,6 +26,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
+const styles = StyleSheet.create({});
