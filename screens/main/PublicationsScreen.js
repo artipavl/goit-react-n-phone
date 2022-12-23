31db3b04774ca8ee62037163cdb64ab4e0b0441f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-23 02:56:13 +0200

    Create PublicationsScreen.js

---
+++ screens/main/PublicationsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const PublicationsScreen = ({
+    params,
+}) => (
+    <View>
+        <Text>PublicationsScreen</Text>
+    </View>
+);
+
+export default PublicationsScreen;
