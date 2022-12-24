b076ad3b98260e9d68f65a7b559fe9f783c05a05
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-24 03:59:14 +0200

    Create CreatePostsScreen.js

---
+++ screens/main/CreatePostsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CreatePostsScreen = ({
+    params,
+}) => (
+    <View>
+        <Text>CreatePostsScreen</Text>
+    </View>
+);
+
+export default CreatePostsScreen;
