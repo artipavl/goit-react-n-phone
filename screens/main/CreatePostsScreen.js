33207c1f014469a76599e99de51b5bf3242853f5
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-27 22:48:21 +0200

    Update CreatePostsScreen.js

--- screens/main/CreatePostsScreen.js
+++ screens/main/CreatePostsScreen.js
@@ -1,12 +1,246 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+import { FontAwesome, Feather } from "@expo/vector-icons";
 
-const CreatePostsScreen = ({
-    params,
-}) => (
-    <View>
-        <Text>CreatePostsScreen</Text>
+const CreatePostsScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(() => Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const onSubmit = () => {
+    console.log(photo, name, location);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          ref={(ref) => {
+            setCameraRef(ref);
+          }}
+        >
+          <TouchableOpacity
+            style={styles.flipContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+                console.log(uri);
+                setPhoto(uri);
+              }
+            }}
+          >
+            <FontAwesome name="camera" size={24} color="#BDBDBD" />
+          </TouchableOpacity>
+        </Camera>
+      </View>
+      <View>
+        <View>
+          <Text style={styles.photoInfortation}>
+            {photo ? "Редактировать фото" : "Загрузите фото"}
+          </Text>
+        </View>
+        <SafeAreaView style={{ marginTop: 32 }}>
+          <View>
+            <TextInput
+              style={styles.input}
+              value={name}
+              onChangeText={setName}
+              autoComplete="name"
+              placeholder="Название..."
+              placeholderTextColor="#BDBDBD"
+            />
+          </View>
+          <View style={{ position: "relative", marginTop: 16 }}>
+            <View
+              style={{
+                position: "absolute",
+                top: 13,
+                left: 0,
+                width: 24,
+                height: 24,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Feather name="map-pin" size={18} color="#BDBDBD" />
+            </View>
+
+            <TextInput
+              style={{
+                ...styles.input,
+                paddingLeft: 28,
+              }}
+              value={location}
+              onChangeText={setLocation}
+              //   autoComplete=""
+              placeholder="Местность..."
+              placeholderTextColor="#BDBDBD"
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.subButton}
+            onPress={onSubmit}
+            activeOpacity="0.8"
+          >
+            <Text style={styles.buttonText}>Опубликовать</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
+      <TouchableOpacity
+        style={styles.trashButton}
+        // onPress={}
+      >
+        <Feather name="trash-2" size={24} color="#BDBDBD" />
+      </TouchableOpacity>
     </View>
-);
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 32,
+    paddingBottom: 32,
+  },
+  cameraContainer: {
+    borderWidth: 1,
+    overflow: "hidden",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    width: "100%",
+    height: 240,
+    marginBottom: 8,
+  },
+  camera: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    position: "relative",
+  },
+
+  flipContainer: {
+    position: "absolute",
+    top: 15,
+    right: 15,
+    flex: 1,
+  },
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    height: 60,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 50,
+  },
+  photoInfortation: {
+    fontFamily: "Roboto-400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+  },
+  subButton: {
+    // display: 1,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: 32,
+    // backgroundColor: "#FF6C00",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 100,
+  },
+  buttonText: {
+    fontFamily: "Roboto-400",
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    // color: "#FFFFFF",
+    color: "#BDBDBD",
+  },
+
+  trashButton: {
+    fles: 1,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    height: 40,
+    // backgroundColor: "#FF6C00",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 100,
+  },
+  input: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+    fontFamily: "Roboto-400",
+    color: "#212121",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+});
 
 export default CreatePostsScreen;
