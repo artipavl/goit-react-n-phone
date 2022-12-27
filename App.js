dbdd5aaf2d0fbc9afd7e2aadccf7da019c7f536f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-27 22:47:29 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Feather, AntDesign, Octicons } from "@expo/vector-icons";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -17,9 +18,12 @@ SplashScreen.preventAutoHideAsync();
 import LoginScreen from "./screens/auth/LoginScreen";
 import RegisterScreen from "./screens/auth/RegisterScreen";
 import { Button, StyleSheet, Text, View } from "react-native";
-import PostsScreen from "./screens/main/PostsScreen";
 import ProfileScreen from "./screens/main/ProfileScreen";
 import CreatePostsScreen from "./screens/main/CreatePostsScreen";
+import Home from "./screens/main/Home";
+import { Provider } from "react-redux";
+import Main from "./components/Main";
+import { store } from "./redux/store";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -41,105 +45,20 @@ export default function App() {
   onLayoutRootView();
 
   return (
-    <NavigationContainer>
-      {isLogin ? (
-        <MainTab.Navigator>
-          <MainTab.Screen
-            name="Публикации"
-            component={PostsScreen}
-            options={{
-              tabBarStyle: styles.tabBar,
-              tabBarShowLabel: false,
-              tabBarIcon: ({ color, size }) => (
-                <AntDesign name="appstore-o" color={color} size={size} />
-              ),
-              headerTintColor: "#212121",
-              headerTitleStyle: {
-                fontWeight: "Roboto-500",
-              },
-              headerTitleAlign: "center",
-              headerRight: () => (
-                  <Feather
-                    name="log-out"
-                    size={24}
-                    color="black"
-                    // onPress={}
-                  />
-                )
-              ,
-            }}
-          />
-          <MainTab.Screen
-            name="Создать публикацию"
-            component={CreatePostsScreen}
-            options={{
-              tabBarShowLabel: false,
-              tabBarShow: false,
-
-              tabBarIcon: ({ color, size }) => (
-                <View style={styles.buttonPlus}>
-                  <Octicons name="plus" color={color} size={size} />
-                </View>
-              ),
-              tabBarStyle: { display: "none" },
-              headerLeft: () => {
-                const navigation = useNavigation();
-                return (
-                  <Feather
-                    name="arrow-left"
-                    size={24}
-                    color="black"
-                    onPress={() => navigation.goBack()}
-                  />
-                );
-              },
-              headerTintColor: "#212121",
-              headerTitleStyle: {
-                fontWeight: "Roboto-500",
-              },
-              headerTitleAlign: "center",
-            }}
-          />
-          <MainTab.Screen
-            name="Профиль"
-            component={ProfileScreen}
-            options={{
-              headerShown: false,
-              tabBarShowLabel: false,
-              tabBarIcon: ({ color, size }) => (
-                <Feather name="user" color={color} size={size} />
-              ),
-            }}
-          />
-        </MainTab.Navigator>
-      ) : (
-        <AuthStack.Navigator>
-          <AuthStack.Screen
-            name="Register"
-            component={RegisterScreen}
-            options={{ headerShown: false }}
-          />
-          <AuthStack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-        </AuthStack.Navigator>
-      )}
-    </NavigationContainer>
+   <Provider store = {store}><Main /></Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  buttonPlus: {
-    backgroundColor: "#FF6C00",
-    width: 70,
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 100,
-  },
-  tabBar: {
-    // backgroundColor: "red",
-  },
-});
+// const styles = StyleSheet.create({
+//   buttonPlus: {
+//     backgroundColor: "#FF6C00",
+//     width: 70,
+//     height: 40,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     borderRadius: 100,
+//   },
+//   tabBar: {
+//     // backgroundColor: "red",
+//   },
+// });
