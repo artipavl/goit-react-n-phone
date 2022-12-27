67ad189ef4817c2192bc5d592ef1765bd3f51e28
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-27 22:47:54 +0200

    Create store.js

---
+++ redux/store.js
@@ -0,0 +1,10 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authReduser";
+
+const rootReducer = combineReducers({
+  [authSlice.name]: authSlice.reducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
