ccfde42b828b9cd2725f573b7bdcaa4a83d94e46
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-27 22:47:47 +0200

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAryDnuf5gS5gREzOz2Hq9uCTwAlASeKUM",
+  authDomain: "you-gallery-rn.firebaseapp.com",
+  projectId: "you-gallery-rn",
+  storageBucket: "you-gallery-rn.appspot.com",
+  messagingSenderId: "316350288170",
+  appId: "1:316350288170:web:3f451a215efc9597bc4e53",
+  measurementId: "G-N8C3D4H0EN",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
