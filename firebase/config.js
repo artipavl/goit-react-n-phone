d035494cd2d6d801f747cf29a5effd9c7ed2b733
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-30 05:05:12 +0200

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +18,7 @@ const firebaseConfig = {
   messagingSenderId: "316350288170",
   appId: "1:316350288170:web:3f451a215efc9597bc4e53",
   measurementId: "G-N8C3D4H0EN",
+  // databaseURL: "https://you-gallery-rn-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
@@ -21,3 +26,15 @@ export const app = initializeApp(firebaseConfig);
 // export default firebase.initializeApp(firebaseConfig);
 
 // const analytics = getAnalytics(app);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage();
+
+// Initialize
+export const auth = getAuth();
+
+// Initialize Realtime Database and get a reference to the service
+// const database = getDatabase();
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore();
